16de3414c9e625f4611a65e84755bb38dbe43e6e
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-02-04 19:27:10 +0100

    feat: delete city from the list

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -58,7 +58,24 @@ function CitiesProvider({ children }) {
 
       setCities((cities) => [...cities, data]);
     } catch (error) {
-      alert("There was an error loading data...")
+      alert("There was an error creating the city...")
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(
+        `${BASE_URL}/cities/${id}`, {
+          method : "DELETE"
+        }
+      );
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (error) {
+      alert("There was an error deleting the city...")
     } finally {
       setIsLoading(false)
     }
@@ -71,7 +88,8 @@ function CitiesProvider({ children }) {
     isLoading,
     currentCity,
     getCity,
-    createCity
+    createCity,
+    deleteCity
   }}>
     {children}
   </CitiesContext.Provider>
